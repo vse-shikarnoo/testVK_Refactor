d06a173d6e242455aaba36c8b027ea0d62ad728a
Klushin Valdimir <vova_klushin@mail.ru>
2024-05-09 16:07:29 +0300

    task 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:2.28.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("org.mockito:mockito-android:2.24.5")
 }
---
+++ app/src/test/java/com/vk/usersapp/UserListFeatureTest.kt
@@ -0,0 +1,10 @@
+package com.vk.usersapp
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class UserListFeatureTest {
+
+
+}
